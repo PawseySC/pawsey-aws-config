e8bfdf80a5accc1eb1216dcd553b39f0103858b2
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2024-10-07 13:58:00 +0800

    Updates to quantum hack related scripts and material

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Pawsey AWS related configuration scripts 
+
+This repo contains scripts to deploy AWS EC2 instances. 
--- scripts/aws_quantum_hackathon_setup.sh
+++ scripts/aws_quantum_hackathon_setup.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+# update default packages 
+sudo apt update 
+sudo apt -y --no-install-recommends install python3-pip python3-virtualenv jupyterhub
+
+
+# clone appropriate repo
+git clone https://github.com/PawseySC/pawsey-aws-config 
+# set requirements 
+# these are based on having pennylane qiskit qmuvi seaborn matplotlib installed
+export qhackreqs=pawsey-aws-config/qhack/python/requirements.txt
+
+
+# create the users 
+create_jupyterhub_users.py 2 $(hostname) > userlist.txt 
+
+# create virtual environment 
+python3 -m venv qhack-env
+source qhack-env/bin/activate 
+pip install -r ${qhacksreq} 
+deactivate 
+
 # run "The littlest jupyter hub" script
 # the script will setup a jupyter hub with the following 
-curl -L https://tljh.jupyter.org/bootstrap.py \
-| sudo python3 - 
---user-requirements-txt-url \
+wget -L https://tljh.jupyter.org/bootstrap.py 
+sudo python3 bootstrap.py \
+--user-requirements-txt-url ${qhackreqs} \
 --show-progress-page \
+--admin qhack-admin
+
+# now copy templates to the juptyer up region 
+cp -r pawsey-aws-config/qhack/templates #need destination directory
